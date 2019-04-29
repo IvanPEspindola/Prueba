087d407fcb8b4fb50a91479a5b784f539b65dfd1
Leo Badia <badialeo.96@gmail.com>
2019-04-29 18:28:06 -0300

    Ahora si makefiles andando

--- sockets/sockets.h
+++ sockets/sockets.h
@@ -1,9 +1,13 @@
 #ifndef SOCKETS_H_
 #define SOCKETS_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include<string.h>
+#include <string.h>
 
 typedef enum
 {
